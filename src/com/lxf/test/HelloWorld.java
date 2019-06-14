8c0cfae34238fc36ed7181dfcde39aad82cc9935
luozi <291956946@qq.com>
2019-06-14 18:04:39 +0800

    第一次提交项目

---
+++ src/com/lxf/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lxf.test;
+
+/**
+ * @author Da Luo Zi
+ * @data 2019/6/14 17:55
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
